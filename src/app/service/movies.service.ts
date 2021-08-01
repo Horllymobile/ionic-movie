fda8cbe97e4914ec584ee1825939a784ba24c1a2
Abraham James Olamide <45019164+Horllymobile@users.noreply.github.com>
2021-08-01 17:55:06 +0100

    Added service for fetching movies and also icons

---
+++ src/app/service/movies.service.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/member-ordering */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable max-len */
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  basedUrl = 'https://api.themoviedb.org/3/movie/popular';
+  private apiKey = environment.apiKey;
+  private page = 1;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get pageGet() {
+    return this.page;
+  }
+
+  set pageSet(pageIndex: number) {
+    this.page = pageIndex;
+  }
+
+  getMovies(page: number): Observable<any>{
+    if(page > this.page){
+      this.page = page;
+      return this.http.get(`${this.basedUrl}/?page=${page}&&api_key=${this.apiKey}`);
+    }
+    return this.http.get(`${this.basedUrl}/?page=${page}&&api_key=${this.apiKey}`);
+  }
+}
